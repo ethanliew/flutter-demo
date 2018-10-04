814d05914169b7e352e411a070469a2c7f38f71a
Ethan Liew <ethanliew.now@gmail.com>
2018-10-04 13:37:20 +0800

    change app name to codename generator create list view layout to hold the code name

--- lib/RandomWords.dart
+++ lib/RandomWords.dart
@@ -1,16 +1,50 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/widgets.dart'; //  building word list widget aka control
+import 'package:flutter/material.dart'; //  applying material design
+import 'package:english_words/english_words.dart'; // using random english word package
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
+
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    //    final wordPair = WordPair.random();
+    //    return Text(wordPair.asPascalCase);
+
+    //    return Scaffold(
+    //      appBar: AppBar(
+    //        title: Text('Codename Generator'),
+    //      ),
+    //      body: _buildSuggestions(),
+    //    );
+
+    return Scaffold(body: _buildSuggestions());
   }
-  // TODO Add build method
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider(); // insert list item separator
+          final index = i ~/ 2; // it returns integer result
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,49 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     //final wordPair = WordPair.random();
 
+//    return MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+//          // child: Text('Hello World'),
+//          child: Text(wordPair.asPascalCase),
+//        ),
+//      ),
+//    );
+
+//    return MaterialApp(
+//      title: 'Codename Generator',
+//      home: RandomWords(),
+//    );
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-         // child: Text('Hello World'),
+        home: new Scaffold(
+            appBar: new AppBar(
+              leading: new Icon(Icons.person),
+              title: new Text('Codename Generator'),
+            ),
+            body: new Center(child: new RandomWords()),
 
-          child: RandomWords(),
+//            persistentFooterButtons: <Widget>[
+//          new FlatButton(
+//            child: new Icon(Icons.add),
+//            onPressed: null,
+//          ),
+//          new FlatButton(
+//            child: new Icon(Icons.edit),
+//            onPressed: null,
+//          ),
+//          new FlatButton(
+//            child: new Icon(Icons.delete),
+//            onPressed: null,
+//          ),
+//        ]
 
-        ),
-      ),
-    );
+        ));
   }
 }
